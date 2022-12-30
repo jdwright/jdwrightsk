cccaa3fb86290006c9601c4ffbb79f5c53a82a6f
Jonathan Wright <jdwright@ldc.upenn.edu>
2022-12-30 03:03:15 -0500

    auth

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,13 +3,15 @@ import { getServerSession } from '@supabase/auth-helpers-sveltekit'
 import { supabase } from '$lib/supabaseClient'
 export async function handle({ event, resolve }) {
   const session = await getServerSession(event);
-  const { data, error, status } = await supabase
-    .from('profiles')
-    .select('*')
-    .eq('id', session.user.id)
-    .single();
-  // event.locals.user = await getUserInformation(event.cookies.get('sessionid'));
-  event.locals.user = data;
+  if(session){
+    const { data, error, status } = await supabase
+      .from('profiles')
+      .select('*')
+      .eq('id', session.user.id)
+      .single();
+    // event.locals.user = await getUserInformation(event.cookies.get('sessionid'));
+    event.locals.user = data;
+  }
 
   const response = await resolve(event);
   // response.headers.set('x-custom-header', 'potato');
