162f0932f8c6aa5f6101fee27511e0d2d10dc678
Jonathan Wright <jdwright@ldc.upenn.edu>
2022-12-29 21:43:37 -0500

    auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,18 @@
-import '$lib/supabaseClient'
+// import '$lib/supabaseClient'
+import { getServerSession } from '@supabase/auth-helpers-sveltekit'
+import { supabase } from '$lib/supabaseClient'
+export async function handle({ event, resolve }) {
+  const session = await getServerSession(event);
+  const { data, error, status } = await supabase
+    .from('profiles')
+    .select('*')
+    .eq('id', session.user.id)
+    .single();
+  // event.locals.user = await getUserInformation(event.cookies.get('sessionid'));
+  event.locals.user = data;
+
+  const response = await resolve(event);
+  // response.headers.set('x-custom-header', 'potato');
+
+  return response;
+}
